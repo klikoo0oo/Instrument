2cacf014354e7d58ceecee9a0faaaa1e64ba00c7
Даниил Пронин <daniilpronin250505@mail.ru>
2023-02-07 23:24:56 +0300

    Добавьте файлы проекта.

---
+++ Instrument/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+interface Instrument
+{
+    public void play()
+    {
+        string KEY = "До мажор";
+    }
+}
+
+class Guitar : Instrument
+{
+    public byte Strings = 6;
+    public void play()
+    {
+        Console.WriteLine($"Гитара с {Strings} Струнами играет ");
+    }
+}
+
+class Drum : Instrument
+{
+    public byte Size = 6;
+    public void play()
+    {
+        Console.WriteLine($"Игрет барабан с размером {Size}");
+    }
+}
+
+class Pipe : Instrument
+{
+    public byte Diameter = 6;
+    public void play()
+    {
+        Console.WriteLine($"Играет труба с диаметром {Diameter}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Instrument[] instruments = { new Guitar(), new Pipe(), new Pipe() };
+        foreach (Instrument instument in instruments)
+        {
+            instument.play();
+        }
+    }
+}
+
